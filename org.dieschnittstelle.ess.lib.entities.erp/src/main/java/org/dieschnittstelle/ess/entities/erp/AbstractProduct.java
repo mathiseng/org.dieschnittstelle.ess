6b7534a782e91af32fbd53496dbc9ce04e43918d
men <mathis.eng@novatec-gmbh.de>
2024-05-28 12:17:14 +0200

    MIP-JPA3

--- org.dieschnittstelle.ess.lib.entities.erp/src/main/java/org/dieschnittstelle/ess/entities/erp/AbstractProduct.java
+++ org.dieschnittstelle.ess.lib.entities.erp/src/main/java/org/dieschnittstelle/ess/entities/erp/AbstractProduct.java
@@ -3,6 +3,9 @@ package org.dieschnittstelle.ess.entities.erp;
 import java.io.Serializable;
 
 import com.fasterxml.jackson.annotation.JsonTypeInfo;
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.Id;
 import org.apache.logging.log4j.Logger;
 import org.dieschnittstelle.ess.entities.GenericCRUDEntity;
 import org.dieschnittstelle.ess.utils.jsonb.JsonbJsonTypeInfoHandler;
@@ -20,6 +23,7 @@ import static org.dieschnittstelle.ess.utils.jsonb.JsonbJsonTypeInfoHandler.KLAS
  * concrete subclass of the abstract class, and thus allows to create correctly typed java objects
  * based on the untyped json data.
  */
+@Entity
 @JsonTypeInfo(use=JsonTypeInfo.Id.CLASS, include=JsonTypeInfo.As.PROPERTY, property=KLASSNAME_PROPERTY)
 @JsonbTypeDeserializer(JsonbJsonTypeInfoHandler.class)
 @JsonbTypeSerializer(JsonbJsonTypeInfoHandler.class)
@@ -28,14 +32,16 @@ public abstract class AbstractProduct implements Serializable, GenericCRUDEntity
 	protected static Logger logger = org.apache.logging.log4j.LogManager.getLogger(AbstractProduct.class);
 
 	/**
-	 * 
+	 *
 	 */
 	private static final long serialVersionUID = 6940403029597060153L;
 
+	@Id
+    @GeneratedValue
 	private long id;
 
 	private String name;
-	
+
 	private int price;
 
 	public AbstractProduct() {
@@ -61,7 +67,7 @@ public abstract class AbstractProduct implements Serializable, GenericCRUDEntity
 	public void setName(String name) {
 		this.name = name;
 	}
-	
+
 	public int getPrice() {
 		return price;
 	}
--- org.dieschnittstelle.ess.mip.client/src/main/java/org/dieschnittstelle/ess/mip/client/apiclients/StockSystemClient.java
+++ org.dieschnittstelle.ess.mip.client/src/main/java/org/dieschnittstelle/ess/mip/client/apiclients/StockSystemClient.java
@@ -9,55 +9,58 @@ import org.dieschnittstelle.ess.entities.erp.IndividualisedProductItem;
 
 public class StockSystemClient implements StockSystem {
 
-	private StockSystemService serviceProxy;
+    private StockSystemService serviceProxy;
 
-	public StockSystemClient() throws Exception {
-		// TODO: remove the comments and complete the implementation
-//		this.serviceProxy = ServiceProxyFactory.getInstance().getProxy(null);
-	}
+    public StockSystemClient() throws Exception {
+        // TODO: remove the comments and complete the implementation
+        this.serviceProxy = ServiceProxyFactory.getInstance().getProxy(StockSystemService.class);
+    }
 
-	// TODO: uncomment the commented sections from all the following methods and remove the default return statements
+    // TODO: uncomment the commented sections from all the following methods and remove the default return statements
 
-	@Override
-	public void addToStock(IndividualisedProductItem product, long pointOfSaleId, int units) {
-//		this.serviceProxy.addToStock(product.getId(),pointOfSaleId,units);
-	}
+    @Override
+    public void addToStock(IndividualisedProductItem product, long pointOfSaleId, int units) {
+		this.serviceProxy.addToStock(product.getId(),pointOfSaleId,units);
+    }
 
-	@Override
-	public void removeFromStock(IndividualisedProductItem product, long pointOfSaleId,
-								int units) {
-//		this.serviceProxy.removeFromStock(product.getId(),pointOfSaleId,units);
-	}
+    @Override
+    public void removeFromStock(IndividualisedProductItem product, long pointOfSaleId,
+                                int units) {
+		this.serviceProxy.removeFromStock(product.getId(),pointOfSaleId,units);
+    }
 
-	@Override
-	public List<IndividualisedProductItem> getProductsOnStock(long pointOfSaleId) {
-		return this.serviceProxy.getProductsOnStock(pointOfSaleId);
+    //Produkte die für gegebene Verkaufsstelle vorhanden sind
+    @Override
+    public List<IndividualisedProductItem> getProductsOnStock(long pointOfSaleId) {
+        return this.serviceProxy.getProductsOnStock(pointOfSaleId);
 //		return new ArrayList<>();
-	}
+    }
 
-	@Override
-	public List<IndividualisedProductItem> getAllProductsOnStock() {
-//		return this.serviceProxy.getProductsOnStock(0);
-		return new ArrayList<>();
-	}
+    //Wenn posId nicht gesetzt , dann diese Methode aufrufen, ansonsten getProductsOnStock
+    @Override
+    public List<IndividualisedProductItem> getAllProductsOnStock() {
+		return this.serviceProxy.getProductsOnStock(0);
+//        return new ArrayList<>();
+    }
 
-	@Override
-	public int getUnitsOnStock(IndividualisedProductItem product, long pointOfSaleId) {
-//		return this.serviceProxy.getUnitsOnStock(product.getId(),pointOfSaleId);
-		return 0;
-	}
+    @Override
+    public int getUnitsOnStock(IndividualisedProductItem product, long pointOfSaleId) {
+		return this.serviceProxy.getUnitsOnStock(product.getId(),pointOfSaleId);
+ //       return 0;
+    }
 
-	@Override
-	public int getTotalUnitsOnStock(IndividualisedProductItem product) {
-//		return this.serviceProxy.getUnitsOnStock(product.getId(),0);
-		return 0;
-	}
+    // Wo gibt es das Produkt in welcher Anzahl
+    @Override
+    public int getTotalUnitsOnStock(IndividualisedProductItem product) {
+		return this.serviceProxy.getUnitsOnStock(product.getId(),0);
+ //       return 0;
+    }
 
-	@Override
-	public List<Long> getPointsOfSale(IndividualisedProductItem product) {
-//		return this.serviceProxy.getPointsOfSale(product.getId());
-		return new ArrayList<>();
-	}
+    @Override
+    public List<Long> getPointsOfSale(IndividualisedProductItem product) {
+		return this.serviceProxy.getPointsOfSale(product.getId());
+  //      return new ArrayList<>();
+    }
 
 
 }
