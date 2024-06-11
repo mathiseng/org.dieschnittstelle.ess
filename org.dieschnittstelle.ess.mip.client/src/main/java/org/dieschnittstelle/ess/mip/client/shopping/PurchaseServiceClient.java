f380b359f03af9913172c9806123a5bc1827358f
men <mathis.eng@novatec-gmbh.de>
2024-06-11 12:03:08 +0200

    PAT1

--- org.dieschnittstelle.ess.mip.client/src/main/java/org/dieschnittstelle/ess/mip/client/shopping/PurchaseServiceClient.java
+++ org.dieschnittstelle.ess.mip.client/src/main/java/org/dieschnittstelle/ess/mip/client/shopping/PurchaseServiceClient.java
@@ -1,47 +1,68 @@
 package org.dieschnittstelle.ess.mip.client.shopping;
 
 import org.apache.logging.log4j.Logger;
+import org.dieschnittstelle.ess.entities.erp.Campaign;
+import org.dieschnittstelle.ess.entities.shopping.ShoppingCartItem;
+import org.dieschnittstelle.ess.mip.client.apiclients.ServiceProxyFactory;
+import org.dieschnittstelle.ess.mip.client.apiclients.ShoppingCartClient;
+import org.dieschnittstelle.ess.mip.components.shopping.api.PurchaseService;
 import org.dieschnittstelle.ess.mip.components.shopping.api.ShoppingException;
 import org.dieschnittstelle.ess.entities.crm.AbstractTouchpoint;
 import org.dieschnittstelle.ess.entities.crm.Customer;
 import org.dieschnittstelle.ess.entities.erp.AbstractProduct;
+import org.dieschnittstelle.ess.mip.components.shopping.cart.api.ShoppingCart;
 
 public class PurchaseServiceClient implements ShoppingBusinessDelegate {
 
-	protected static Logger logger = org.apache.logging.log4j.LogManager
-			.getLogger(PurchaseServiceClient.class);
+    protected static Logger logger = org.apache.logging.log4j.LogManager
+            .getLogger(PurchaseServiceClient.class);
 
-	/*
-	 * TODO PAT1: use an appropriate proxy for the server-side purchase service
-	 *  Note that touchpoint and customer need to be stored locally
-	 *  before purchase() is invoked. For accessing shopping cart data use a local ShoppingCartClient
-	 *  in this case and access the shopping cart using the provided getter method
-	 */
+    /*
+     * TODO PAT1: use an appropriate proxy for the server-side purchase service
+     *  Note that touchpoint and customer need to be stored locally
+     *  before purchase() is invoked. For accessing shopping cart data use a local ShoppingCartClient
+     *  in this case and access the shopping cart using the provided getter method
+     */
 
-	public PurchaseServiceClient() {
-		/* TODO: instantiate the proxy using the ServiceProxyFactory (see the other client classes) */
-	}
+    private AbstractTouchpoint touchpoint;
 
-	/* TODO: implement the following methods s */
+    private Customer customer;
 
-	@Override
-	public void setTouchpoint(AbstractTouchpoint touchpoint) {
-	
-	}
+    private ShoppingCartClient shoppingCartClient;
 
-	@Override
-	public void setCustomer(Customer customer) {
-	
-	}
+    private PurchaseService purchaseServiceProxy;
 
-	@Override
-	public void addProduct(AbstractProduct product, int units) {
-	
-	}
+    public PurchaseServiceClient() {
+        try {
 
-	@Override
-	public void purchase() throws ShoppingException {
-	
-	}
+            /* TODO: instantiate the proxy using the ServiceProxyFactory (see the other client classes) */
+            purchaseServiceProxy = ServiceProxyFactory.getInstance().getProxy(PurchaseService.class);
+            this.shoppingCartClient = new ShoppingCartClient();
+        } catch (Exception e) {
+            throw new RuntimeException("Got exeption: " + e,e);
+        }
+    }
+
+    /* TODO: implement the following methods s */
+
+    @Override
+    public void setTouchpoint(AbstractTouchpoint touchpoint) {
+        this.touchpoint = touchpoint;
+    }
+
+    @Override
+    public void setCustomer(Customer customer) {
+        this.customer = customer;
+    }
+
+    @Override
+    public void addProduct(AbstractProduct product, int units) {
+        this.shoppingCartClient.addItem(new ShoppingCartItem(product.getId(), units, product instanceof Campaign));
+    }
+
+    @Override
+    public void purchase() throws ShoppingException {
+        this.purchaseServiceProxy.purchaseCartAtTouchpointForCustomer(this.shoppingCartClient.getShoppingCartEntityId(), touchpoint.getId(), customer.getId());
+    }
 
 }
--- org.dieschnittstelle.ess.mip.components.shopping.api/src/main/java/org/dieschnittstelle/ess/mip/components/shopping/api/PurchaseService.java
+++ org.dieschnittstelle.ess.mip.components.shopping.api/src/main/java/org/dieschnittstelle/ess/mip/components/shopping/api/PurchaseService.java
@@ -1,8 +1,17 @@
 package org.dieschnittstelle.ess.mip.components.shopping.api;
 
 // TODO: PAT1: this is the interface to be provided as a rest service if rest service access is used
+
+import jakarta.ws.rs.*;
+import jakarta.ws.rs.core.MediaType;
+
+@Consumes(MediaType.APPLICATION_JSON)
+@Produces(MediaType.APPLICATION_JSON)
+@Path("/shoppingcarts")
 public interface PurchaseService {
 
-	void purchaseCartAtTouchpointForCustomer(long shoppingCartId, long touchpointId, long customerId) throws ShoppingException;
-	
+    @POST
+    @Path("/{cartId}/purchase")
+    void purchaseCartAtTouchpointForCustomer(@PathParam("cartId") long shoppingCartId, @QueryParam("tpId") long touchpointId, @QueryParam("custId") long customerId) throws ShoppingException;
+
 }
